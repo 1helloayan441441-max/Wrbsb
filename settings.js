ed496b51610ae91318f0dc76b63ad23036ee3f51
1helloayan441441-max <1helloayan441441@gmail.com>
2026-07-12 11:51:43 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '923042264448',
+    botName: 'AYAN-6V MD',
+    ownerName: 'AYAN6V'
+};
